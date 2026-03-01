f889324984d31725693354997be924fb53db8f50
Dustin Smith <dustin.william.smith@gmail.com>
2026-03-01 14:46:16 +0700

    feat(v2): Terraform DynamoDB tables (control, joblog, rerun)

---
+++ deploy/v2/terraform/dynamodb.tf
@@ -0,0 +1,77 @@
+resource "aws_dynamodb_table" "control" {
+  name         = "${var.environment}-interlock-control"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  tags = var.tags
+}
+
+resource "aws_dynamodb_table" "joblog" {
+  name         = "${var.environment}-interlock-joblog"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  tags = var.tags
+}
+
+resource "aws_dynamodb_table" "rerun" {
+  name         = "${var.environment}-interlock-rerun"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  tags = var.tags
+}
